4336a9c69c09badfcdcfea2360d118b2a863a5f5
Miguel <miguevalencia29@hotmail.com>
2020-06-19 22:12:12 -0500

    Probando el cambio de tema

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./App.css";
 
 function App() {
+  const handleChangeColor = () => {
+    document.body.classList.toggle("is-dark-mode");
+  };
+
   return (
     <div className="App">
       <p>FTGYUIJ</p>
+      <button onClick={handleChangeColor}>Cambiar Color</button>
     </div>
   );
 }
