f4cab1990bd6151f7967ed9f4c1ae3fd6f1d04a1
Miguel <miguevalencia29@hotmail.com>
2020-06-23 15:17:20 -0500

    Agregando el header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   const handleChangeColor = () => {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <p>FTGYUIJ</p>
+      <Header />
       <button onClick={handleChangeColor}>Cambiar Color</button>
     </div>
   );
