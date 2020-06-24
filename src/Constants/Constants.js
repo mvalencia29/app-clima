a02361242955aced94ad5e0fedf8bf7ed8ce1a34
Miguel <miguevalencia29@hotmail.com>
2020-06-24 14:03:46 -0500

    Agregando Constantes

---
+++ src/Constants/Constants.js
@@ -0,0 +1,20 @@
+//Api Key Battuta Medunes
+const apiKeyBattutaMedunes = "64ec6719decce6d7d27967711d04d84c";
+
+//Api Key Openweathermap
+const apiKeyOpenweathermap = "64ec6719decce6d7d27967711d04d84c";
+
+//Url to get Countries
+export const urlApiGetCountries = `http://battuta.medunes.net/api/country/all/?key=${apiKeyBattutaMedunes}`;
+
+//Url to get Cities
+export const urlApiGetCities = (codeCountry, region) =>
+  `http://battuta.medunes.net/api/city/${codeCountry}/search/?region=${region}&key=${apiKeyBattutaMedunes}`;
+
+//Url to get Regions
+export const urlApiGetRegions = () =>
+  `http://battuta.medunes.net/api/country/all/?key=${apiKeyBattutaMedunes}`;
+
+//Url to get the weather
+export const urlApiGetWeather = (city, countryCode) =>
+  `http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKeyOpenweathermap}`;
---
+++ src/services/Cities.js
@@ -0,0 +1,11 @@
+export async function callApiCities() {
+  try {
+        
+  } catch (error) {}
+}
+
+export async function analyzeResponseApiCities() {
+    try {
+  
+    } catch (error) {}
+  }
