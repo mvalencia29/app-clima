b3964542c5f7f8d20c9766a2822c5c57c4a9e216
Miguel <miguevalencia29@hotmail.com>
2020-06-24 00:21:27 -0500

    Agregando Archivos e imagen

--- src/components/Main/Form/Styles.js
+++ src/components/Main/Form/Styles.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import Button from "@material-ui/core/Button";
 
 export const FormContainer = styled.section`
   border-radius: 5px;
   background: #fff;
   border: 1px solid rgba(0, 0, 0, 0.125);
-  padding: 20px;
+  padding: 10px 20px 25px 20px;
 `;
